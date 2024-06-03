f598d6e905eb06c36821bc90c26a3e38eeea72a0
mathy <matiwordgit@gmail.com>
2024-06-03 17:36:44 -0300

    formulario Carrera terminado

--- src/main/java/ar/edu/unju/fi/collections/ListadoCarreras.java
+++ src/main/java/ar/edu/unju/fi/collections/ListadoCarreras.java
@@ -1,59 +1,62 @@
 package ar.edu.unju.fi.collections;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import ar.edu.unju.fi.model.Carrera;
 
 public class ListadoCarreras {
+	
+	public static List<Carrera> carreras = new ArrayList<Carrera>();
+	
+	// Método para listar carreras
+		  public static List<Carrera> listarCarreras() {
+			  //buscar solo los que tengan estado en true
+			  
+		    return carreras;
+		  }
 
-		public static List<Carrera> carerras = new ArrayList<Carrera>();
-		
-		//Metodo para listar carreras		
-		public static List<Carrera> listarCarreras(){
-			return carerras;
-		}
-		
-		//Metodo para buscar carrera por id
-		public static Carrera buscarCarreraPorCodigo(String codigo) {
-		    for (Carrera c : carerras) {
+		  // Método para buscar una carrera por ID
+		  public static Carrera buscarCarreraPorCodigo(String codigo) {
+		    for (Carrera c : carreras) {
 		      if (c.getCodigo().equals(codigo)) {
 		        return c;
 		      }
 		    }
 		    return null;
 		  }
-		
+
 		  // Método para agregar una carrera
 		  public static void agregarCarrera(Carrera c) {
 			//agregando el estado
 			c.setEstado(true);
-			carerras.add(c);
+		    carreras.add(c);
 		  }
-		
+
 		  // Método para modificar una carrera
 		  public static void modificarCarrera(Carrera carreraModificada) {
-		    for (int i = 0; i < carerras.size(); i++) {
-		      Carrera carrera = carerras.get(i);
+		    for (int i = 0; i < carreras.size(); i++) {
+		      Carrera carrera = carreras.get(i);
 		      if (carrera.getCodigo().equals(carreraModificada.getCodigo())) {
-		    	  carerras.set(i, carreraModificada);
+		        carreras.set(i, carreraModificada);
 		        break;
 		      }
 		    }
 		  }
-			
-			  // Método para eliminar una carrera
-			  public static void eliminarCarrera(String codigo) {
-				  //borrado físico
-			    //carreras.removeIf(carrera -> carrera.getCodigo().equals(codigo));
-				  
-				  //borrado lógico
-				  for (int i = 0; i < carerras.size(); i++) {
-				      Carrera carrera = carerras.get(i);
-				      if (carrera.getCodigo().equals(codigo)) {
-				        carrera.setEstado(false);;
-				        break;
-				      }
-				    }
-			  }
-		
+
+		  // Método para eliminar una carrera
+		  public static void eliminarCarrera(String codigo) {
+			  //borrado físico
+		    //carreras.removeIf(carrera -> carrera.getCodigo().equals(codigo));
+			  
+			  //borrado lógico
+			  for (int i = 0; i < carreras.size(); i++) {
+			      Carrera carrera = carreras.get(i);
+			      if (carrera.getCodigo().equals(codigo)) {
+			        carrera.setEstado(false);;
+			        break;
+			      }
+			    }
+		  }
+
 }
--- src/main/java/ar/edu/unju/fi/controller/CarreraController.java
+++ src/main/java/ar/edu/unju/fi/controller/CarreraController.java
@@ -2,19 +2,33 @@ package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unju.fi.collections.ListadoCarreras;
 import ar.edu.unju.fi.model.Carrera;
 
 @Controller
 public class CarreraController {
-	@GetMapping()
-	public ModelAndView getFormularioCarrera() { //Manejo de html y objetos
-		//vosta formCarrerDocente.html
-		ModelAndView modelView = new ModelAndView("formCarrerDocente");
-		//agrega el objeto
-		modelView.addObject(new Carrera());
-		
-		return modelView;
-	}
+    
+    @GetMapping("/formularioCarrera")
+    public ModelAndView getFormularioCarrera() {
+        ModelAndView modeloVista = new ModelAndView("formCarrer");
+        // Agrega el objeto con el nombre adecuado
+        modeloVista.addObject("nuevaCarrera", new Carrera());
+        return modeloVista;
+    }
+    
+    @PostMapping("/guardarCarrera")
+    public ModelAndView saveCarrera(@ModelAttribute("nuevaCarrera") Carrera carreraParaGuardar) {
+        // Guardar
+        ListadoCarreras.agregarCarrera(carreraParaGuardar);
+        
+        // Mostrar el listado
+        ModelAndView modelView = new ModelAndView("listaDeCarreras");
+        modelView.addObject("listadoCarreras", ListadoCarreras.listarCarreras());    
+        
+        return modelView;        
+    }
 }
